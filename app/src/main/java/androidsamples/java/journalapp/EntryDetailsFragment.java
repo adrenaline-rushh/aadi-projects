4ae6122d9e91a377585ffd7d2302313b048654a8
adrenaline-rushh <raghavanaaditya07@gmail.com>
2023-12-01 09:41:31 +0530

    UI persistence sorted

--- app/src/main/java/androidsamples/java/journalapp/EntryDetailsFragment.java
+++ app/src/main/java/androidsamples/java/journalapp/EntryDetailsFragment.java
@@ -37,6 +37,10 @@ public class EntryDetailsFragment extends Fragment {
   private EditText mTitle;
   private Button mDate, mStart, mEnd;
   private String entryId;
+  private static final String DATE_TAG = "date";
+  private static final String START_TIME_TAG = "start time";
+  private static final String END_TIME_TAG = "end time";
+  private static final String ENTRY_ID = "entry ID";
 
   @NonNull
   public static EntryDetailsFragment newInstance() {
@@ -60,6 +64,8 @@ public class EntryDetailsFragment extends Fragment {
       entryDetailsViewModel.loadEntry(entryId);
     }
 
+
+
     Log.d(TAG, "Loading entry: " + entryId);
   }
 
@@ -77,15 +83,93 @@ public class EntryDetailsFragment extends Fragment {
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
+    Log.d(TAG, "EntryDetails onViewCreated called");
     mTitle = view.findViewById(R.id.edit_title);
     mDate = view.findViewById(R.id.btn_entry_date);
     mStart = view.findViewById(R.id.btn_start_time);
     mEnd = view.findViewById(R.id.btn_end_time);
 
-    if(savedInstanceState == null){
-      mDate.setText(R.string.date);
-      mStart.setText(R.string.start_time);
-      mEnd.setText(R.string.end_time);
+//    if(savedInstanceState == null){
+//      mDate.setText(R.string.date);
+//      mStart.setText(R.string.start_time);
+//      mEnd.setText(R.string.end_time);
+//    }
+//    else
+//    {
+//      Date date = (Date)savedInstanceState.getSerializable(DATE_TAG);
+//      if (date!=null)
+//    }
+
+    entryId = EntryDetailsFragmentArgs.fromBundle(getArguments()).getEntryId();
+    entryDetailsViewModel.entryId = entryId;
+
+
+
+      if (savedInstanceState != null) {
+        Date savedDate = (Date) savedInstanceState.getSerializable(DATE_TAG);
+
+        if (savedDate != null) {
+          SimpleDateFormat formatter = new SimpleDateFormat("E, MMM dd, yyyy", Locale.ENGLISH);
+          mDate.setText(formatter.format(savedDate));
+        } else {
+          mDate.setText(R.string.date);
+        }
+        Date savedStartTime = (Date) savedInstanceState.getSerializable(START_TIME_TAG);
+        if (savedStartTime != null) {
+          SimpleDateFormat formatterTime = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+          mStart.setText(formatterTime.format(savedStartTime));
+        } else
+          mStart.setText(R.string.start_time);
+
+        Date savedEndTime = (Date) savedInstanceState.getSerializable(END_TIME_TAG);
+        if (savedEndTime != null) {
+          SimpleDateFormat formatterTime = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+          mEnd.setText(formatterTime.format(savedEndTime));
+        } else
+          mEnd.setText(R.string.end_time);
+
+      } else {
+        mDate.setText(R.string.date);
+        mStart.setText(R.string.start_time);
+        mEnd.setText(R.string.end_time);
+
+        entryId = EntryDetailsFragmentArgs.fromBundle(getArguments()).getEntryId();
+        entryDetailsViewModel.entryId = entryId;
+
+
+
+
+        if (!entryId.isEmpty()) {
+          Log.d(TAG,"Entry ID not empty");
+          Calendar cal = Calendar.getInstance();
+
+          int date = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedDate();
+          int month = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedMonth();
+          int year = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedYear();
+
+          SimpleDateFormat formatter = new SimpleDateFormat("E, MMM dd, yyyy", Locale.ENGLISH);
+          cal.set(year, month, date);
+          String formattedDate = formatter.format(cal.getTime());
+          mDate.setText(formattedDate);
+
+          String title = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedTitle();
+
+          mTitle.setText(title);
+
+          int startHour = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedStartTimeHour();
+          int startMin = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedStartTimeMinute();
+          int endHour = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedEndTimeHour();
+          int endMin = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedEndTimeMinute();
+
+          mStart.setText(String.format(Locale.ENGLISH, "%02d:%02d", startHour, startMin));
+          mEnd.setText(String.format(Locale.ENGLISH, "%02d:%02d", endHour, endMin));
+
+          entryDetailsViewModel.title = mTitle.getText().toString();
+          entryDetailsViewModel.date1 = mDate.getText().toString();
+          entryDetailsViewModel.start = mStart.getText().toString();
+          entryDetailsViewModel.end = mEnd.getText().toString();
+        }
+
     }
 
     mDate.setOnClickListener(this::setDate);
@@ -93,40 +177,40 @@ public class EntryDetailsFragment extends Fragment {
     mEnd.setOnClickListener(this::setEndTime);
     view.findViewById(R.id.btn_save).setOnClickListener(this::saveEntry);
 
-    entryId = EntryDetailsFragmentArgs.fromBundle(getArguments()).getEntryId();
-    entryDetailsViewModel.entryId = entryId;
-
-    if(!entryId.isEmpty()) {
-
-      Calendar cal = Calendar.getInstance();
-
-      int date = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedDate();
-      int month = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedMonth();
-      int year = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedYear();
-
-      SimpleDateFormat formatter = new SimpleDateFormat("E, MMM dd, yyyy", Locale.ENGLISH);
-      cal.set(year, month, date);
-      String formattedDate = formatter.format(cal.getTime());
-      mDate.setText(formattedDate);
-
-      String title = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedTitle();
-
-      mTitle.setText(title);
-
-      int startHour = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedStartTimeHour();
-      int startMin = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedStartTimeMinute();
-      int endHour = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedEndTimeHour();
-      int endMin = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedEndTimeMinute();
-
-      mStart.setText(String.format(Locale.ENGLISH, "%02d:%02d", startHour, startMin));
-      mEnd.setText(String.format(Locale.ENGLISH, "%02d:%02d", endHour, endMin));
-
-      entryDetailsViewModel.title = mTitle.getText().toString();
-      entryDetailsViewModel.date1 = mDate.getText().toString();
-      entryDetailsViewModel.start = mStart.getText().toString();
-      entryDetailsViewModel.end = mEnd.getText().toString();
-
-    }
+//    entryId = EntryDetailsFragmentArgs.fromBundle(getArguments()).getEntryId();
+//    entryDetailsViewModel.entryId = entryId;
+//
+//    if(!entryId.isEmpty()) {
+//      Log.d(TAG,"Entry ID empty");
+//      Calendar cal = Calendar.getInstance();
+//
+//      int date = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedDate();
+//      int month = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedMonth();
+//      int year = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedYear();
+//
+//      SimpleDateFormat formatter = new SimpleDateFormat("E, MMM dd, yyyy", Locale.ENGLISH);
+//      cal.set(year, month, date);
+//      String formattedDate = formatter.format(cal.getTime());
+//      mDate.setText(formattedDate);
+//
+//      String title = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedTitle();
+//
+//      mTitle.setText(title);
+//
+//      int startHour = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedStartTimeHour();
+//      int startMin = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedStartTimeMinute();
+//      int endHour = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedEndTimeHour();
+//      int endMin = EntryDetailsFragmentArgs.fromBundle(getArguments()).getSelectedEndTimeMinute();
+//
+//      mStart.setText(String.format(Locale.ENGLISH, "%02d:%02d", startHour, startMin));
+//      mEnd.setText(String.format(Locale.ENGLISH, "%02d:%02d", endHour, endMin));
+//
+//      entryDetailsViewModel.title = mTitle.getText().toString();
+//      entryDetailsViewModel.date1 = mDate.getText().toString();
+//      entryDetailsViewModel.start = mStart.getText().toString();
+//      entryDetailsViewModel.end = mEnd.getText().toString();
+//
+//    }
 
   }
 
@@ -191,6 +275,62 @@ public class EntryDetailsFragment extends Fragment {
 //  }
 
   public void updateUI(){
+
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState)
+  {
+    Log.d(TAG,"onSaveInstanceState called");
+    super.onSaveInstanceState(outState);
+    SimpleDateFormat formatter = new SimpleDateFormat("E, MMM dd, yyyy", Locale.ENGLISH);
+
+    SimpleDateFormat formatterTime = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+
+
+    if (mDate!=null)
+    {
+      try
+        {
+          Date entryDate = formatter.parse(mDate.getText().toString());
+          outState.putSerializable(DATE_TAG,entryDate);
+        } catch (ParseException P)
+        {
+          Log.d("ERROR", "onSaveInstanceState: " + P.getMessage());
+        }
+
+    }
+    if (mStart!=null)
+    {
+      try {
+
+        Date entryStartTime = (formatterTime.parse(mStart.getText().toString()));
+        outState.putSerializable(START_TIME_TAG,entryStartTime);
+
+      } catch (ParseException e)
+      {
+        Log.d("ERROR", "onSaveInstanceState: " + e.getMessage());
+      }
+    }
+
+
+    //Date entryStartTime = (formatterTime.parse(mStart.getText().toString()));
+
+    if (mEnd!=null)
+    {
+      try {
+
+        Date entryEndTime = (formatterTime.parse(mEnd.getText().toString()));
+        outState.putSerializable(END_TIME_TAG,entryEndTime);
+
+      } catch (ParseException e)
+      {
+        Log.d("ERROR", "onSaveInstanceState: " + e.getMessage());
+      }
+    }
+    if (entryId!=null)
+      outState.putString(ENTRY_ID,entryId);
+
 
   }
 
